22526c507da1cb0470a0a018fe1758709a598ab5
IQUELFergal <fergal.iquel@gmail.com>
2020-07-11 00:05:56 +0200

    add color randomizer

---
+++ Assets/Scripts/ControlRandomizer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlRandomizer : MonoBehaviour
+{
+    public Color green = Color.green;
+    public Color red = Color.red;
+    bool isRed = true;
+
+    public Image image;
+    public Button button;
+
+
+    void Start()
+    {
+        image.color = red;
+    }
+    public void ChangeColor()
+    {
+        if (isRed)
+        {
+            image.color = green;
+            
+        }
+        else image.color = red;
+        isRed = !isRed;
+    }
+}
