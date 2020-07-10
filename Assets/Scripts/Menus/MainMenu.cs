e9c3c5924f71f6857d7daf5f288a02451897a6e6
IQUELFergal <fergal.iquel@gmail.com>
2020-07-10 23:16:38 +0200

    add menus

---
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame(SceneAsset scene)
+    {
+        SceneManager.LoadScene(scene.name);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
+
