89b2c77e61440e829314ae844140e53695afc879
IQUELFergal <fergal.iquel@gmail.com>
2020-07-11 13:15:41 +0100

    Add interaction

---
+++ Assets/Scripts/Interaction System/Interactors/PlayerRaycastInteractor.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+
+public class PlayerRaycastInteractor : MonoBehaviour
+{
+	public InteractionUI interactionUI = null;
+	public Transform startingPoint;
+
+	public float distance = 1f;
+	public KeyCode interactKey;
+	public KeyCode moveKey;
+
+	public Material defaultMaterial;
+	public Material outlineMaterial;
+	GameObject obj;
+
+
+	private void Start()
+	{
+		if (startingPoint == null)
+		{
+			Debug.LogError("No starting point found for the raycast : Assigning this gameObject's transform...");
+			startingPoint = transform;
+		}
+	}
+
+
+	// Update is called once per frame
+	void Update()
+	{
+		Physics2D.queriesStartInColliders = false;
+		RaycastHit2D hit = Physics2D.Raycast(startingPoint.position, transform.GetComponent<PlayerController>().GetLastInputDir(), distance);
+		Debug.DrawRay(startingPoint.position, transform.GetComponent<PlayerController>().GetLastInputDir(), Color.red);
+
+		if (hit.collider != null && (hit.collider.GetComponent<Movable>() != null || hit.collider.GetComponent<IInteractable>() != null))
+		{
+			GameObject tmp = hit.collider.gameObject;
+			if (tmp != obj)
+			{
+				if (obj != null) obj.GetComponent<SpriteRenderer>().material = defaultMaterial;
+				obj = tmp;
+			}
+			obj.GetComponent<SpriteRenderer>().material = outlineMaterial;
+
+			//Movable
+			if (hit.collider.GetComponent<Movable>() != null)
+			{
+				//Start moving
+				if (Input.GetKeyDown(moveKey))
+				{
+					obj = hit.collider.gameObject;
+					obj.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+					obj.GetComponent<FixedJoint2D>().connectedBody = this.GetComponent<Rigidbody2D>();
+					obj.GetComponent<FixedJoint2D>().enabled = true;
+					obj.GetComponent<Movable>().beingPushed = true;
+				}
+			}
+			//End moving
+			if (Input.GetKeyUp(moveKey) && obj != null && obj.GetComponent<Movable>().beingPushed)
+			{
+				obj.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+				obj.GetComponent<FixedJoint2D>().enabled = false;
+				obj.GetComponent<Movable>().beingPushed = false;
+				obj = null;
+			}
+
+			//Interactable
+			else if (hit.collider.GetComponent<IInteractable>() != null)
+			{
+				if (Input.GetKeyDown(interactKey))
+				{
+					obj.GetComponent<IInteractable>().Interact();
+				}
+			}
+		}
+		else if (obj != null) obj.GetComponent<SpriteRenderer>().material = defaultMaterial;
+	}
+}
---
+++ Assets/Scripts/Interaction System/PressurePlate.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PressurePlate : MonoBehaviour
+{
+    public IInteractable[] interactables;
+    public Collider2D trigger;
+    // Start is called before the first frame update
+    void Start()
+    {
+        trigger = GetComponent<Collider2D>();
+        if (trigger == null)
+        {
+            Debug.LogError("No collider2D found : creating a new one...");
+            trigger = gameObject.AddComponent<BoxCollider2D>();
+        }
+        trigger.isTrigger = true;
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        for (int i = 0; i < interactables.Length; i++)
+        {
+            interactables[i].Interact();
+        }
+    }
+}
