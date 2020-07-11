f457b1a1bf3ba7b994531ef4e40361a7c7c100d2
Landuré Raphaël <raph.landure@gmail.com>
2020-07-11 16:14:39 +0200

    Duplicate the scene

--- Assets/Scripts/PlayerController/SeflDestroy.cs
+++ Assets/Scripts/PlayerController/SeflDestroy.cs
@@ -7,18 +7,11 @@ public class SeflDestroy : MonoBehaviour
 {
 
     public GameObject start;
-    Vector2 startPosition;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        startPosition = start.transform.position;
-    }
 
     public void Destroy()
     {
         gameObject.SetActive(false);
-        transform.position = startPosition;
+        transform.position = start.transform.position;
         gameObject.SetActive(true);
     }
 }
