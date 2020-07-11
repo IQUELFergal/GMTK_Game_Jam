2bccac69c2e507fa90429973b5392d4878a42b11
IQUELFergal <fergal.iquel@gmail.com>
2020-07-11 02:03:23 +0200

    update controlRandomizer

--- Assets/Scripts/ControlRandomizer.cs
+++ Assets/Scripts/ControlRandomizer.cs
@@ -1,28 +1,82 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class ControlRandomizer : MonoBehaviour
 {
     public Color green = Color.green;
     public Color red = Color.red;
+    public Color baseColor = Color.white;
     bool isRed = true;
 
     public Image image;
-    public Button button;
-
+    public Controller[] controllers;
+    
 
     void Start()
     {
-        image.color = red;
+        image.color = baseColor;
+
+        int enumLength = Enum.GetNames(typeof(Controller.Control)).Length;
+        if (enumLength > controllers.Length)
+        {
+            Debug.Log("Not enough controllers for the current enum of controls");
+            return;
+        }
+        
+        RandomizeControllers();
     }
+
+
+    //Debug
     public void ChangeColor()
     {
         if (isRed)
         {
-            image.color = green;
-            
+            StartCoroutine(FlashColor(green));
         }
-        else image.color = red;
+        else StartCoroutine(FlashColor(red));
         isRed = !isRed;
     }
+
+    public IEnumerator FlashColor(Color color)
+    {
+        image.color = color;
+        yield return new WaitForSeconds(.5f);
+        image.color = baseColor;
+    }
+
+    //End of Debug
+
+
+
+
+    void RandomizeControllers()
+    {
+        Debug.Log("Randomizing controls");
+
+        int enumLength = Enum.GetNames(typeof(Controller.Control)).Length;
+        
+        //Creer list de controllers a partir de l'array
+        List<Controller> controllerList = new List<Controller>();
+        for (int i = 0; i < controllers.Length; i++)
+        {
+            controllerList.Add(controllers[i]);
+        }
+
+        for (int i = 1; i < enumLength; i++)
+        {
+            int n = UnityEngine.Random.Range(0, controllerList.Count);
+            controllerList[n].Setup(i);
+            controllerList.RemoveAt(n);
+        }
+
+        for (int i = 0; i < controllerList.Count; i++)
+        {
+            controllerList[i].Setup(0);
+        }
+    }
+
 }
---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine;
+using System;
+
+public class Controller : UIBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    public enum Control { none, moveLeft, moveRight, jump, crouch, interact };
+    public Control control;
+    public Text text;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("Clicked on " + this.ToString());
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("Exiting " + this.ToString());
+    }
+
+    void IPointerEnterHandler.OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Entering " + this.ToString());
+    }
+
+    public void Setup(int i)
+    {
+        if (i >= 0 && i < Enum.GetNames(typeof(Control)).Length)
+        {
+            control = (Control)i;
+            text.text = i.ToString() + " " + control.ToString();
+        }
+        else Debug.LogError("Can't setup the controller with this value : out of bounds");
+    }
+}
